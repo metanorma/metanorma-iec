22d71abb66b679f72629c2f59db2c739fadf884c
Nick Nicholas <opoudjis@gmail.com>
2020-06-29 23:24:15 +1000

    i18n refactor: https://github.com/metanorma/metanorma-standoc/issues/285

--- lib/isodoc/iec/base_convert.rb
+++ lib/isodoc/iec/base_convert.rb
@@ -52,21 +52,6 @@ module IsoDoc
         out.p(**{ class: "zzSTDTitle1" }) { |p| p << "&nbsp;" }
       end
 
-      def load_yaml(lang, script)
-        y = if @i18nyaml then YAML.load_file(@i18nyaml)
-            elsif lang == "en"
-              YAML.load_file(File.join(File.dirname(__FILE__), "i18n-en.yaml"))
-            elsif lang == "fr"
-              YAML.load_file(File.join(File.dirname(__FILE__), "i18n-fr.yaml"))
-            elsif lang == "zh" && script == "Hans"
-              YAML.load_file(File.join(File.dirname(__FILE__),
-                                       "i18n-zh-Hans.yaml"))
-            else
-              YAML.load_file(File.join(File.dirname(__FILE__), "i18n-en.yaml"))
-            end
-        super.merge(y)
-      end
-
       def convert1(docxml, filename, dir)
         id = docxml&.at(ns("//bibdata/docnumber"))&.text
         @is_iev = id == "60050"
