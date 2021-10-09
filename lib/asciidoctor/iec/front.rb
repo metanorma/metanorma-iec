0cd40515681f174ed4c48ddbead2f332f1858f39
Nick Nicholas <opoudjis@gmail.com>
2021-10-10 03:43:53 +1100

    moved obsoletes to ISO: https://github.com/metanorma/metanorma-iso/issues/534

--- lib/asciidoctor/iec/front.rb
+++ lib/asciidoctor/iec/front.rb
@@ -205,10 +205,6 @@ module Asciidoctor
         a = node.attr("function") and xml.function a
       end
 
-      def relaton_relations
-        super + %w(obsoletes)
-      end
-
       def metadata_ext(node, xml)
         super
         a = node.attr("accessibility-color-inside") and
