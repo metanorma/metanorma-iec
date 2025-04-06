76586cbc3393abd92df20802f2e4d3317840663e
Nick Nicholas <opoudjis@gmail.com>
2025-04-06 13:21:22 +1000

    schema testing: https://github.com/metanorma/metanorma/issues/437

--- spec/metanorma/validate_spec.rb
+++ spec/metanorma/validate_spec.rb
@@ -216,4 +216,18 @@ RSpec.describe Metanorma::Iec do
     expect(File.read("test.err.html"))
       .not_to include("Illegal document stage: 60.60")
   end
+
+  it "validates document against Metanorma XML schema" do
+    Asciidoctor.convert(<<~"INPUT", *OPTIONS)
+      = A
+      X
+      :docfile: test.adoc
+      :no-pdf:
+
+      [align=mid-air]
+      Para
+    INPUT
+    expect(File.read("test.err.html"))
+      .to include('value of attribute "align" is invalid; must be equal to')
+  end
 end
